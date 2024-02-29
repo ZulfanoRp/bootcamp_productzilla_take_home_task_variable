acf3b117b20a48ce951c08e9461bdfd317e1c0c4
ZulfanoRp <zulfano67@gmail.com>
2024-02-29 22:29:37 +0700

    push varible1.js & variable2.js

---
+++ variable1.js
@@ -0,0 +1,59 @@
+let angka1 = 5 - 4 - 3 - 2 - 1;
+console.log(angka1);
+
+let angka2 = 10 * 2 / 5 ** 2 - 1;
+console.log(angka2);
+
+let angka3 = 11111 / 2 - 3333 * 2 + 5555;
+console.log(angka3);
+
+let angka4 = 9 ** 2 / 3 - 4 * 2;
+console.log(angka4);
+
+let angka5 = 10001 + 20002 - 30003 * 0.5 / 2 ** 2;
+console.log(angka5);
+
+let angka6 = 3 ** 2 - 6 + 9 * 2 / 3;
+console.log(angka6);
+
+let angka7 = 12345 / 3 + 98765 - 11111 * 2;
+console.log(angka7);
+
+let angka8 = 15 * 2 - 30 / 2 ** 2 + 75 / 3;
+console.log(angka8);
+
+let angka9 = 88888 / 4 - 77777 * 2 + 66666;
+console.log(angka9);
+
+let angka10 = 20 + 40 * 2 / 10 - 100 / 5;
+console.log(angka10);
+
+let angka11 = 111 - 222 * 3 + 333 / 2 ** 2;
+console.log(angka11);
+
+let angka12 = 987 / 3 + 654 - 321 * 2;
+console.log(angka12);
+
+let angka13 = 111 / 3 * 2 - 333 + 555 ** 0.5;
+console.log(angka13);
+
+let angka14 = 121 * 2 - 144 / 2 + 169 ** 0.5;
+console.log(angka14);
+
+let angka15 = 246 / 2 + 369 * 2 - 135 / 3;
+console.log(angka15);
+
+let angka16 = 888 - 999 / 111 + 222 * 3 ** 0.5;
+console.log(angka16);
+
+let angka17 = 1000 * 2 - 2000 / 4 + 3000 / 3;
+console.log(angka17);
+
+let angka18 = 1111 / 2 + 2222 * 3 - 3333 / 3;
+console.log(angka18);
+
+let angka19 = 999 / 3 - 888 * 2 + 777 ** 0.5;
+console.log(angka19);
+
+let angka20 = 50 ** 2 / 100 + 150 / 3 - 200 * 0.5;
+console.log(angka20);
---
+++ variable2.js
@@ -0,0 +1,68 @@
+// Operator ">"
+let perbandingan1 = 10 > 5;
+console.log("10 > 5:", perbandingan1);
+
+let perbandingan2 = 10 > 20;
+console.log("10 > 20:", perbandingan2);
+
+// Operator "<"
+let perbandingan3 = 10 < 5;
+console.log("10 < 5:", perbandingan3);
+
+let perbandingan4 = 10 < 20;
+console.log("10 < 20:", perbandingan4);
+
+// Operator ">="
+let perbandingan5 = 10 >= 5;
+console.log("10 >= 5:", perbandingan5);
+
+let perbandingan6 = 10 >= 10;
+console.log("10 >= 10:", perbandingan6);
+
+// Operator "<="
+let perbandingan7 = 10 <= 5;
+console.log("10 <= 5:", perbandingan7);
+
+let perbandingan8 = 10 <= 10;
+console.log("10 <= 10:", perbandingan8);
+
+// Operator "=="
+let perbandingan9 = 10 == 5;
+console.log("10 == 5:", perbandingan9);
+
+let perbandingan10 = 10 == 10;
+console.log("10 == 10:", perbandingan10);
+
+// Operator "!="
+let perbandingan11 = 10 != 5;
+console.log("10 != 5:", perbandingan11);
+
+let perbandingan12 = 10 != 10;
+console.log("10 != 10:", perbandingan12);
+
+// Operator "==="
+let perbandingan13 = 10 === "10";
+console.log('10 === "10":', perbandingan13);
+
+let perbandingan14 = 10 === 10;
+console.log("10 === 10:", perbandingan14);
+
+// Operator "!=="
+let perbandingan15 = 10 !== "10";
+console.log('10 !== "10":', perbandingan15);
+
+let perbandingan16 = 10 !== 10;
+console.log("10 !== 10:", perbandingan16);
+
+let perbandingan17 = 10 > 5 > 20 < 30;
+console.log("10 > 5 > 20 < 30:", perbandingan17);
+
+let perbandingan18 = 10 > 5 > 20 > 10;
+console.log("10 > 5 > 20 > 10:", perbandingan18);
+
+let perbandingan19 = 10 > 5 < 20 < 10;
+console.log("10 > 5 < 20 < 10:", perbandingan19);
+
+let perbandingan20 = 10 < 5 < 20 < 10;
+console.log("10 < 5 < 20 < 10:", perbandingan20);
+
